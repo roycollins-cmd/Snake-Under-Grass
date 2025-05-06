0c45d9a6f426d9b9251df7b550ab693eaed2412f
Roy Kucherera <132816040+roycollins-cmd@users.noreply.github.com>
2025-05-06 14:22:46 +0700

    added comments

--- the collatz sequence.py
+++ the collatz sequence.py
@@ -3,18 +3,19 @@ The collatz sequence AKA the simplest impossible math problem
 """
 
 def collatz(number):
+    #check if the number is even
     if number % 2 == 0:
-        for i in range(1, number):
+        for i in range(1, number): #print loop to show each number after every calculation
             number = number // 2
             if number == 1:
-                break
+                break # stop when the number is 1 - otherwise it prints 0 up to the number
             print(number)
         return number
-    else:
-        for i in range(1, number):
+    else: #check if the number is odd (not even)
+        for i in range(1, number): #print loop to show each number after every calculation
             number = number * 3 + 1
             if number == 1:
-                break
+                break  # stop when the number is 1 - otherwise it prints 0 up to the number
             print(number)
         return number
 
